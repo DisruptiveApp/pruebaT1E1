bf2d545e1ec1a40541202baff1ca66a823e0dcc3
Luis Quiñonez <luisquinonez@macbook-pro-de-luis.local>
2016-03-01 01:27:22 -0600

    Pruebas repositorio

--- T1E1LuisQuiñonez/ViewController.swift
+++ T1E1LuisQuiñonez/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("hola mundo")
+        print("Hola Mundo")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
